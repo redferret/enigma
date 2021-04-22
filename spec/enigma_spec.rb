e10bae5d19c7b8885a84d7491fc2db33c9aacdf8
Richard <rdesilvey@gmail.com>
2021-04-22 14:41:34 -0600

    Test that it exists

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -5,7 +5,8 @@ require './lib/enigma'
 RSpec.describe Enigma do
   describe '#new' do
     it 'Tests that it exists' do
-      
+      enigma = Enigma.new
+      expect(enigma).to be_an Enigma
     end
   end
 end
