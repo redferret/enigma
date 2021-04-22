51e270d8c6b2690c747a3ac0c4170eacbbb2ba62
Richard <rdesilvey@gmail.com>
2021-04-22 15:29:21 -0600

    Implement method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,3 +3,23 @@ class Enigma
     @character_set = ("a".."z").to_a << " "
   end
 
+
+  def generate_key
+    random_number = random(99999)
+    if random_number < 10000
+      # add padding
+      random_number_as_s = random_number.to_s
+      diff = 5 - random_number_as_s.length
+      padding = ""
+      diff.times do
+        padding << '0'
+      end
+      return "#{padding}#{random_number_as_s}"
+    end
+    random_number.to_s
+  end
+
+  private
+  def random(range)
+    rand(range)
+  end
