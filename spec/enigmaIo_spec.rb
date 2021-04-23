82abb60d6e5163bd0a3f10d2fc794ca8a7c3c8aa
Richard <rdesilvey@gmail.com>
2021-04-22 20:17:52 -0600

    Fix misspelled vars

--- spec/enigmaIo_spec.rb
+++ spec/enigmaIo_spec.rb
@@ -4,8 +4,8 @@ require './lib/enigmaIo'
 RSpec.describe EnigmaIo do
   describe '#new' do
     it 'tests that EnigmaIo exists' do
-      enigmaIo = Enigmaio.new
-      expect(enigmaIO).is_a? EnigmaIo
+      enigmaIo = EnigmaIo.new
+      expect(enigmaIo).is_a? EnigmaIo
     end
   end
 end
