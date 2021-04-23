793d2a9c35bfcc70b1f9f4cac700ee2aaeb08b1d
Richard <rdesilvey@gmail.com>
2021-04-22 22:02:22 -0600

    Fix error checking

--- encrypt.rb
+++ encrypt.rb
@@ -1,7 +1,17 @@
 require './lib/enigma'
+require 'date'
 
 def valid_date?(date)
-  /\d{6}/.match? date
+  return (/\d{6}/.match? date) && check_date(date)
+end
+
+def check_date(date)
+  begin
+    Date.strptime(date, '%d%m%y')
+  rescue ArgumentError
+    return false
+  end
+  return true
 end
 
 def valid_key?(key)
@@ -9,13 +19,22 @@ def valid_key?(key)
 end
 
 def valid_number_of_arguments?(args)
-  args.length == 4
+  args.length >= 2 && args.length < 5
 end
 
-def arguments_are_valid(arguments)
-  date = arguments[3]
-  key = arguments[2]
-  return valid_date?(date) && valid_key?(key) && valid_number_of_arguments?(arguments)
+def arguments_are_valid?(arguments)
+  if valid_number_of_arguments?(arguments)
+    arg_length = arguments.length
+    if arg_length == 2
+      # files only
+      return true
+    elsif arg_length == 3
+      return valid_key?(arguments[2])
+    elsif arg_length == 4
+      return valid_key?(arguments[2]) && valid_date?(arguments[3])
+    end
+  end
+  return false
 end
 
 def find_error(arguments)
@@ -23,10 +42,16 @@ def find_error(arguments)
   puts '- Wrong number of arguments given' if wrong_number_of_arguments
   return if wrong_number_of_arguments
 
-  date = arguments[3]
-  key = arguments[2]
-  puts '- Invalid date entered' if not(valid_date? date)
-  puts '- Invalid key given' if not(valid_key? key)
+  arg_length = arguments.length
+  if arg_length == 2
+    # files only
+    return true
+  elsif arg_length == 4
+    puts '- Invalid key given' if not(valid_key? arguments[2])
+    puts '- Invalid date entered' if not(valid_date? arguments[3])
+  elsif arg_length == 3
+    puts '- Invalid key given' if not(valid_key? arguments[2])
+  end
 end
 
 puts "Press Return to quit the program\n\nEnter the following to Encrypt a message"
@@ -38,8 +63,6 @@ loop do
   input = gets.chomp
   break if input == ''
   arguments = input.split(' ')
-
-  break if arguments_are_valid(arguments)
-  puts "Invalid arguments provided\n"
-  find_error(arguments)
+  puts "Invalid arguments provided\n" if not(arguments_are_valid? arguments)
+  find_error(arguments) if not(arguments_are_valid? arguments)
 end
