6bc246ac0a39d150d144232f53eba77e76fcff89
Richard <rdesilvey@gmail.com>
2021-04-25 21:29:24 -0600

    Add decrypt runner

---
+++ decrypt.rb
@@ -0,0 +1,39 @@
+require 'date'
+
+require './lib/enigma'
+require './lib/iocrypt'
+require './lib/validation'
+
+arguments = ARGV
+arg_length = arguments.length
+
+enigma = Enigma.new
+error_type = Validation.find_errors(arguments)
+
+begin
+  if error_type.length > 0
+    error_type.each do |error|
+      case error
+        when :file_not_found
+          raise ArgumentError, '- Message file not found'
+        when :wrong_arg_length
+          if arg_length != 4
+            raise ArgumentError, '- Wrong number of arguments given, expected 4'
+          end
+      end
+    end
+  end
+
+  in_file = arguments[0]
+  out_file = arguments[1]
+  key = arguments[2]
+  date = arguments[3]
+  decrypt_io = IoCrypt.new(in_file, out_file)
+  message = decrypt_io.process_message
+  result = enigma.decrypt(message, key, date)
+  decrypt_io.write_message_to_file(result[:encryption])
+
+  puts "Created '#{out_file}' with the key #{result[:key]} and date #{result[:date]}"
+rescue ArgumentError => e
+  puts e.message
+end
