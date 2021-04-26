a415e5e8d2daeaf29624086b7a0b71f5f6b180f3
Richard <rdesilvey@gmail.com>
2021-04-25 21:31:01 -0600

    Implement IoCrypt

---
+++ lib/iocrypt.rb
@@ -0,0 +1,40 @@
+class IoCrypt
+  def initialize(from_file_name, to_file_name)
+    @file_in = open_file(from_file_name)
+    @file_out = open_file(to_file_name, 'w')
+  end
+
+  def open_file(file_name, option = 'r')
+    if not file_name.include?('./')
+      File.new("./#{file_name}", option)
+    else
+      File.new(file_name, option)
+    end
+  end
+
+  def process_message
+    lines = lines_from_message
+    lines.reduce('') do |message, line|
+      message + line
+    end
+  end
+
+  def write_message_to_file(message)
+    lines = message.split('\n')
+    lines.each do |line|
+      write_line_to(@file_out, message)
+    end
+  end
+
+  def self.file_exists?(file_name)
+    File.exist?(file_name)
+  end
+
+  private
+  def write_line_to(file, message)
+    @file_out.puts(line)
+  end
+  def lines_from_message
+    @file_in.readlines
+  end
+end
